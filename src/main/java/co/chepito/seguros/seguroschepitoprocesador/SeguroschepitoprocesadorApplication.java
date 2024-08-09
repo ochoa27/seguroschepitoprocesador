1fa2f18223a6e9680b3f9831122c9abb0b35bc0c
Juan Fernando <wuanter.castano@gmail.com>
2024-08-08 21:25:05 -0500

    Agregar proyecto seguros chepito procesador, spring boot

---
+++ src/main/java/co/chepito/seguros/seguroschepitoprocesador/SeguroschepitoprocesadorApplication.java
@@ -0,0 +1,13 @@
+package co.chepito.seguros.seguroschepitoprocesador;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class SeguroschepitoprocesadorApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(SeguroschepitoprocesadorApplication.class, args);
+	}
+
+}
